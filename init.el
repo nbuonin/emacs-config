9671fde9cd814d0a60c249fea852ec6e4539b9cb
Nick Buonincontri <nbuonin@gmail.com>
2016-08-07 17:34:48 -0400

    adding c headers to auto-complete

--- init.el
+++ init.el
@@ -5,3 +5,8 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) 
+
+;; (add-to-list 'load-path "~/.emacs.d")    ; This may not be appeared if you have already added.
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
