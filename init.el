3ace9b5ed96a0671d53c763fd6fb420bcc3a1d6c
Nick Buonincontri <nbuonin@gmail.com>
2016-08-07 18:13:23 -0400

    Adding mouse support per: http://stackoverflow.com/questions/5710334/how-can-i-get-mouse-selection-to-work-in-emacs-and-iterm2-on-mac

--- init.el
+++ init.el
@@ -10,3 +10,17 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
+
+;; Enable mouse support
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+			      (interactive)
+			      (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+			      (interactive)
+			      (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  )
