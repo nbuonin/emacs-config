e56e4df3c342f63737b5aedd2dc746c1c63e2b6f
Nick Buonincontri <nb2413@columbia.edu>
2016-08-08 16:15:41 -0400

    fixing autosave location

--- init.el
+++ init.el
@@ -32,3 +32,11 @@
 ;; enable line numbers
 (global-linum-mode t)
 (setq linum-format "%2d ")
+
+;; Save all tempfiles in $TMPDIR/emacs$UID/ per emacs wiki: https://www.emacswiki.org/emacs/AutoSave
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix emacs-tmp-dir)
